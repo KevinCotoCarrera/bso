44a13fc84098fb0f38101d9e7f5505f1446e1715
Kevin Coto <kevincoto2002@gmail.com>
2024-09-16 16:16:09 +0400

    feat: login access API

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,7 +1,7 @@
 import { defineAction } from "astro:actions";
 import { z } from "astro:content";
 
-export async function mutateData(method: string, path: string, payload?: any) {
+export async function mutateData(method: string, path: string, SignupPayload?: any) {
   const baseUrl = import.meta.env.STRAPI_URL || "http://localhost:1337";
   const url = new URL(path, baseUrl);
 
@@ -19,7 +19,7 @@ export async function mutateData(method: string, path: string, payload?: any) {
     const response = await fetch(url.href, {
       method: method,
       headers,
-      body: JSON.stringify({ ...payload }),
+      body: JSON.stringify({ ...SignupPayload }),
     });
     const data = await response.json();
     return data;
@@ -29,14 +29,21 @@ export async function mutateData(method: string, path: string, payload?: any) {
   }
 }
 
-interface Payload {
+interface SignupPayload {
 
 	username: string,
     email: string,
 	password: string,
 	
 }
-
+interface LoginPayload {
+	identifier: string,
+	password: string,
+}
+interface LoginData {
+	email: string,
+	password: string,
+}
 export const server = {
   sign_up: defineAction({
     accept: "form",
@@ -48,17 +55,17 @@ export const server = {
 	  password: z.string({message: 'this field has to be filled'})
     }),
 
-    handler: async (formData:  Payload ) => {
+    handler: async (formData:  SignupPayload ) => {
       // insert comments in db
       console.log(formData);
 
-      const payload: Payload = {
+      const Payload: SignupPayload = {
           email: formData.email,
 		  username: formData.username,
 		  password: formData.password
       };
 
-      const responseData = await mutateData("POST", "api/auth/local/register", payload);
+      const responseData = await mutateData("POST", "api/auth/local/register", Payload);
 
       if (!responseData) {
         return {
@@ -80,4 +87,44 @@ export const server = {
       };
     },
   }),
+  sign_in: defineAction({
+	accept: "form",
+    input: z.object({
+      email: z
+        .string({ message: "This field has to be filled." })
+        .email("This is not a valid email."),
+	  password: z.string({message: 'this field has to be filled'})
+    }),
+
+    handler: async (formData: LoginData ) => {
+      // insert comments in db
+      console.log(formData);
+
+      const Payload: LoginPayload = {
+          identifier: formData.email,
+		  password: formData.password
+      };
+
+      const responseData = await mutateData("POST", "api/auth/local", Payload);
+
+      if (!responseData) {
+        return {
+          strapiErrors: null,
+          message: "Ops! Something went wrong. Please try again.",
+        };
+      }
+
+      if (responseData.error) {
+        return {
+          strapiErrors: responseData.error,
+          message: "Failed to Register.",
+        };
+      }
+
+      return {
+        data: responseData,
+        strapiErrors: null,
+      };
+    },
+  })
 };
